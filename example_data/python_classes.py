76282251009b3b4052f86faf16b07c33df7733e7
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-06-02 18:26:30 +0300

    work somehow

--- example_data/python_classes.py
+++ example_data/python_classes.py
@@ -6,3 +6,46 @@ class MyClass(object):
     def function1(self):
         """ function1: """
         pass
+
+    def function3(self):
+        """ function1: """
+        pass
+
+    def function2(self):
+        """ function1: """
+        pass
+
+class MyClass2(object):
+    def __init__(self):
+        """ __init__: """
+        pass
+
+    def function1(self):
+        """ function1: """
+        pass
+
+    def function3(self):
+        """ function1: """
+        pass
+
+    def function2(self):
+        """ function1: """
+        pass
+
+class MyClass3(object):
+    def __init__(self):
+        """ __init__: """
+        pass
+
+    def function1(self):
+        """ function1: """
+        pass
+
+    def function3(self):
+        """ function1: """
+        pass
+
+    def function2(self):
+        """ function1: """
+        pass
+
--- python_to_gliffy.py
+++ python_to_gliffy.py
@@ -6,9 +6,11 @@ import pprint
 from pprint import pprint
 sys.path.append('.')
 import json
+from copy import deepcopy
 
 
 def get_classes(core_modules, prefix):
+    """" class name: list of methods """
     classes = {}  # class: list of functions
     for core_module in core_modules:
         classes_presentation = pyclbr.readmodule(prefix + core_module)
@@ -22,242 +24,98 @@ def to_sting(core_modules, prefix):
     return pprint.pformat(classes)
 
 
-def class_to_gliffy_json(class_name, method_names):
-    # omg here goes magick
-    id_ = 1
-    class_name_repr = class_name_to_gliffy_json(class_name, id_)
-    id_ += 2
-    #pprint(class_name_repr)
-    method_repr = methods_to_gliffy_json(method_names, id_)
-    #pprint(method_repr)
-    objects = [class_name_repr, method_repr]
-    gliffy_json = to_gliffy(objects)
-    return gliffy_json
+class ClassFactory():
+    def __init__(self):
+        self.cls_counter = 0
+        with open('example_data/2classes.json') as doc:
+            self.reference_json = json.load(doc)
+        self.class_template = self.reference_json['stage']['objects'][0]
+        self.generated_classes = []
+
+    def add_class(self, name, attrs, methods):
+        cls = deepcopy(self.class_template)
+        current_id = self.cls_counter * 7
+        # class self id 3 = 0
+        cls['id'] = current_id
+        #### Class
+        child = cls['children'][0]
+        # id = 1
+        child['id'] = current_id + 1
+        #class_id 1 = 2
+        child['children'][0]['id'] = current_id + 2
+        # class id 2 = 2 
+        child['constraints']['constraints'][0]['HeightConstraint']['heightInfo'][0]['id'] = current_id + 2
+
+        #### Attr
+        child = cls['children'][1]
+        # id = 3
+        child['id'] = current_id + 3
+        #class_id 1 = 4
+        child['children'][0]['id'] = current_id + 4
+        # class id 2 = 4
+        child['constraints']['constraints'][1]['HeightConstraint']['heightInfo'][0]['id'] = current_id + 4
+
+        child['constraints']['constraints'][0]['PositionConstraint']['nodeId'] = current_id + 1
+
+        #### Methods
+        child = cls['children'][2]
+        # id = 5
+        child['id'] = current_id + 5
 
 
-def class_name_to_gliffy_json(class_name, id_):
-    # add id_ + 2
-    class_json = { 
-        "children": [ # class name
-            {
-                "children": None,
-                "graphic": {
-                    "Text": {
-                        "hposition": "none",
-                        "html": "<p style=\"text-align: center;\"><span class=\"gliffy-placeholder-text\" style=\"font-family: Arial; font-size: 12px; font-weight: bold; text-decoration: none; line-height: 14px; color: rgb(0, 0, 0);\">" + class_name + "</span></p>",
-                        "overflow": "none",
-                        "paddingBottom": 2,
-                        "paddingLeft": 2,
-                        "paddingRight": 2,
-                        "paddingTop": 2,
-                        "tid": None,
-                        "valign": "top",
-                        "vposition": "none"
-                    },
-                    "type": "Text"
-                },
-                "height": 18,
-                "id": id_ + 1,
-                "lockAspectRatio": False,
-                "lockShape": False,
-                "order": "auto",
-                "rotation": 0,
-                "uid": None,
-                "width": 140,
-                "x": 0,
-                "y": 0
-            }
-        ],
-        "constraints": { # hline
-                        "constraints": [
-                            {
-                                "HeightConstraint": {
-                                    "growParent": True,
-                                    "heightInfo": [
-                                        {
-                                            "id": id_ + 1,
-                                            "magnitude": 1
-                                        }
-                                    ],
-                                    "isMin": False,
-                                    "padding": 0
-                                },
-                                "type": "HeightConstraint"
-                            }
-                        ]
-                       },
-        "graphic": { # hline 2
-                    "Shape": {
-                        "dropShadow": True,
-                        "fillColor": "#FFFFFF",
-                        "gradient": False,
-                        "opacity": 1,
-                        "shadowX": 4,
-                        "shadowY": 4,
-                        "state": 0,
-                        "strokeColor": "#000000",
-                        "strokeWidth": 2,
-                        "tid": "com.gliffy.stencil.rectangle.basic_v1"
-                    },
-                    "type": "Shape"
-                   },
-        "height": 18,
-        "id": id_,
-        "lockAspectRatio": False,
-        "lockShape": False,
-        "order": "auto",
-        "rotation": 0,
-        "uid": None,
-        "width": 140,
-        "x": 0,
-        "y": 0
-    }
-
-    return class_json
-
-def methods_to_gliffy_json(method_names, id_):
-    method_names_str = '<br>'.join(method_names)
-    method_json = { # cl methods
-                   "children": [ # name
-                       { # method
-                        "children": None,
-                        "graphic": {
-                            "Text": {
-                                "hposition": "none",
-                                "html": "<p style=\"text-align: left;\"><span class=\"gliffy-placeholder-text\" style=\"font-family: Arial; font-size: 12px; font-weight: normal; text-decoration: none; line-height: 14px; color: rgb(0, 0, 0);\"> " + method_names_str + " </span></p>",
-                                "overflow": "none",
-                                "paddingBottom": 2,
-                                "paddingLeft": 2,
-                                "paddingRight": 2,
-                                "paddingTop": 2,
-                                "tid": None,
-                                "valign": "top",
-                                "vposition": "none"
-                            },
-                            "type": "Text"
-                        },
-                        "height": 18,
-                        "id": id_ + 1,
-                        "lockAspectRatio": False,
-                        "lockShape": False,
-                        "order": "auto",
-                        "rotation": 0,
-                        "uid": None,
-                        "width": 140,
-                        "x": 0,
-                        "y": 0
-                       }
-                   ],
-                   "constraints": {
-                       "constraints": [
-                           {
-                               "HeightConstraint": {
-                                   "growParent": False,
-                                   "heightInfo": [
-                                       {
-                                           "id": 0,
-                                           "magnitude": 1
-                                       },
-                                       {
-                                           "id": 1,
-                                           "magnitude": -1
-                                       },
-                                       {
-                                           "id": 3,
-                                           "magnitude": -1
-                                       }
-                                   ],
-                                   "isMin": False,
-                                   "padding": 0
-                               },
-                               "type": "HeightConstraint"
-                           },
-                           {
-                               "PositionConstraint": {
-                                   "nodeId": 3,
-                                   "px": 0,
-                                   "py": 1
-                               },
-                               "type": "PositionConstraint"
-                           }
-                       ]
-                   },
-    "graphic": {
-        "Shape": {
-            "dropShadow": True,
-            "fillColor": "#FFFFFF",
-            "gradient": False,
-            "opacity": 1,
-            "shadowX": 4,
-            "shadowY": 4,
-            "state": 0,
-            "strokeColor": "#000000",
-            "strokeWidth": 2,
-            "tid": "com.gliffy.stencil.rectangle.basic_v1"
-        },
-        "type": "Shape"
-    },
-    "height": 39,
-    "id": id_,
-    "lockAspectRatio": False,
-    "lockShape": False,
-    "order": "auto",
-    "rotation": 0,
-    "uid": None,
-    "width": 140,
-    "x": 0,
-    "y": 36
-    }
-    return method_json
+        #class_id 1 = 6
+        child['children'][0]['id'] = current_id + 6
+        # class id 2 = 0
+        child['constraints']['constraints'][1]['PositionConstraint']['nodeId'] = current_id + 3
 
 
+        #all ids
+        cls['constraints']['constraints'][0]['HeightConstraint']['heightInfo'][0]['id'] = current_id + 1
+        cls['constraints']['constraints'][0]['HeightConstraint']['heightInfo'][0]['id'] = current_id + 3
+        cls['constraints']['constraints'][0]['HeightConstraint']['heightInfo'][0]['id'] = current_id + 6
 
-def to_gliffy(objects):
-    out = {
-    "contentType": "application/gliffy+json",
-    "embeddedResources": {
-        "index": 0,
-        "resources": []
-    },
-    "metadata": {
-        "exportBorder": False,
-        "revision": 0,
-        "title": "untitled"
-    },
-    "stage": {
-        "autoFit": True,
-        "background": "#FFFFFF",
-        "drawingGuidesOn": True,
-        "exportBorder": False,
-        "gridOn": True,
-        "height": 0,
-        "lineStyles": {},
-        "maxHeight": 5000,
-        "maxWidth": 5000,
-        "nodeIndex": 0,
-        "objects": objects,
-        "pageBreaksOn": False,
-        "printGridOn": False,
-        "printPaper": "LETTER",
-        "printPortrait": True,
-        "printShrinkToFit": False,
-        "shapeStyles": {},
-        "snapToGrid": True,
-        "textStyles": {},
-        "themeData": None,
-        "width": 0
-    },
-    "version": "1.1"
-    }
-    return out
+        class_name_html = ('<p style="text-align: center;"><span class="gliffy-placeholder-text" '
+    'style="font-family: Arial; font-size: 12px; font-weight: bold; '
+    'text-decoration: none; line-height: 14px; color: rgb(0, 0, '
+    '0);">{}</span></p>').format(name)
+        cls['children'][0]['children'][0]['graphic']['Text']['html'] = class_name_html
+    
+        attr_html = ('<p style="text-align: left;"><span class="gliffy-placeholder-text" style="font-family: Arial; '
+                    'font-size: 12px; font-weight: normal; text-decoration: none; line-height: 14px; color: '
+                     'rgb(0, 0, 0);">\n</span></p>')
+        for attr in attrs:
+            attr_html += ('<p style="text-align: left;"><span class="gliffy-placeholder-text" style="font-family: Arial; '
+                    'font-size: 12px; font-weight: normal; text-decoration: none; line-height: 14px; color: '
+                     'rgb(0, 0, 0);">{}\n</span></p>').format(attr)
+        cls['children'][1]['children'][0]['graphic']['Text']['html'] = attr_html
 
+        methods_html = ('<p style="text-align: left;"><span class="gliffy-placeholder-text" style="font-family: Arial; '
+                    'font-size: 12px; font-weight: normal; text-decoration: none; line-height: 14px; color: '
+                     'rgb(0, 0, 0);">\n</span></p>')
+        for method in methods:
+            methods_html += ('<p style="text-align: left;"><span class="gliffy-placeholder-text" style="font-family: Arial; '
+                    'font-size: 12px; font-weight: normal; text-decoration: none; line-height: 14px; color: '
+                     'rgb(0, 0, 0);">{}\n</span></p>').format(method)
+        cls['children'][2]['children'][0]['graphic']['Text']['html'] = methods_html
+
+
+        cls['x'] = 100 + self.cls_counter * 200
+        self.cls_counter += 1
+        self.generated_classes.append(cls)
+    
+    def write(self, filename):
+        output = deepcopy(self.reference_json)
+        output['stage']['objects'] = self.generated_classes
+        with open(filename, 'w') as outfile:
+            json.dump(output, outfile)
+    
 
 if __name__ == '__main__':
     # core_modules = ['alert_handler',  'alerts',  'bx_interface',  'collector',  'context',  'database',  'devices',  'download',  'emergency_checker',  'nodes_test',  'parameters',  'power_control']
     core_modules = ['python_classes']
     prefix = 'example_data.'
     classes = get_classes(core_modules, prefix)
-    cls = list(classes.keys())[0]
-    gliffy = class_to_gliffy_json(cls, classes[cls])
-    # pprint(gliffy)
-    print(json.dumps(gliffy))
+    factory = ClassFactory()
+    for cls in classes:
+        factory.add_class(cls, ['self'], methods=classes[cls])
+    factory.write('example_data/output2.json')
